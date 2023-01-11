ed9178fcbf490ae3ef92a01e43c9ad5361f1c057
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2023-01-11 07:31:23 +0100

    initial main

---
+++ udemy_sparkforjavadev_1/src/main/java/com/ldi/Main.java
@@ -0,0 +1,34 @@
+package com.ldi;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> inputData = new ArrayList<>();
+        inputData.add(35);
+        inputData.add(12);
+        inputData.add(90);
+        inputData.add(20);
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<Integer> myRdd =  sc.parallelize(inputData);
+        Integer result = myRdd.reduce((value1, value2) -> value1 + value2);
+
+        JavaRDD<Double> sqrtRdd = myRdd.map( value -> Math.sqrt(value) );
+        sqrtRdd.collect().forEach(value -> System.out.println(value));
+
+
+
+
+    }
+}
