fc199367d66d174675a2af03e88f20a3766151e2
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2023-02-17 06:45:32 +0100

    reduceRDD16

---
+++ udemy_sparkforjavadev_1/src/main/java/com/ldi/ReduceRdd16.java
@@ -0,0 +1,43 @@
+package com.ldi;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ReduceRdd16 {
+    public static void main(String[] args) {
+        List<String> inputData = new ArrayList<>();
+        inputData.add("WARN: Tuesday 4 September 0405");
+        inputData.add("ERROR: Tuesday 4 September 0408");
+        inputData.add("FATAL: Wednesday 5 September 1632");
+        inputData.add("ERROR: Friday 7 September 1854");
+        inputData.add("WARN; Saturday 8 September 1942");
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setAppName("reduceRDD example").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
+
+        JavaPairRDD<String, Long> pairRDD = originalLogMessages.mapToPair( rawValue -> {
+            String[] columns = rawValue.split(":");
+            String level = columns[0];
+            return new Tuple2<>(level, 1L);
+
+        }  );
+
+        pairRDD.reduceByKey((value1, value2) -> value1 + value2);
+
+        sc.close();
+
+    }
+}
