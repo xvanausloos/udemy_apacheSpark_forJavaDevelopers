b2e61096466202df9f0d707f6b31a93d8b7828c8
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2023-09-16 17:28:37 +0200

    module 65

--- udemy_sparkforjavadev_1/src/main/java/com/ldi/module_65_SparkSQL_dateformatting.java
+++ udemy_sparkforjavadev_1/src/main/java/com/ldi/module_65_SparkSQL_dateformatting.java
@@ -2,19 +2,25 @@ package com.ldi;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.spark.sql.*;
-import org.apache.spark.sql.types.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/* udemy module 63 Spark SQL in memory RowFactory class is doing this job. Can be useful for unit test  */
-public class module_63_SparkSQL_InMemory {
+/* udemy module 65 Spark SQL date formatting  */
+public class module_65_SparkSQL_dateformatting {
 
     public static void main(String[] args) {
         Logger.getLogger("org.apache").setLevel(Level.WARN);
-        SparkSession spark = SparkSession.builder().appName("module 63").master("local[*]")
+        SparkSession spark = SparkSession.builder().appName("module 65").master("local[*]")
                 .config("spark.sql.warehouse.dir","file:///c:/tmp/")
                 .getOrCreate(); //initiate Spark with Spark SQL different than SparkConf
 
@@ -34,15 +40,11 @@ public class module_63_SparkSQL_InMemory {
         StructType schema = new StructType(fields);
         Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
 
-        dataset.show();
-
-
-
-        //group by level
         dataset.createOrReplaceTempView("logging_table");
-        //Dataset<Row> results = spark.sql("SELECT level, COUNT(datetime) FROM logging_table GROUP BY level ORDER BY level");
-        Dataset<Row> results = spark.sql("SELECT level, collect_list(datetime) FROM logging_table GROUP BY level ORDER BY level");
-        results.show(true);
+        Dataset<Row> results = spark.sql("SELECT level, date_format(datetime, 'M') FROM logging_table");
+
+        results.show();
+
         //hack for keeping Spark UI 4040 running
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
