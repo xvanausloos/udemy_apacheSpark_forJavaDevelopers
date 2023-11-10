a6df4c93e59bbc952ed04c281034df63177ca9e5
Xavier VAN AUSLOOS <xavier.vanausloos@ladonneeintelligente.com>
2023-11-10 12:47:37 +0100

    refactor project

--- src/main/java/com/ldi/module_66_SparkSQL_grouping.java
+++ src/main/java/com/ldi/module_66_SparkSQL_grouping.java
@@ -16,17 +16,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 /* udemy module 65 Spark SQL date formatting  */
 public class module_66_SparkSQL_grouping {
 
     public static void main(String[] args) {
         Logger.getLogger("org.apache").setLevel(Level.WARN);
-        SparkSession spark = SparkSession.builder().appName("module 66 multiple groupings").master("local[*]")
-                .config("spark.sql.warehouse.dir","file:///c:/tmp/")
+        SparkSession spark = SparkSession.builder()
+                .appName("module 66 multiple groupings")
+                .master("local[*]")
+                .config("spark.sql.warehouse.dir","/tmpspark")
                 .getOrCreate(); //initiate Spark with Spark SQL different than SparkConf
 
-        /*
-        STAGE 1 inMemory
+
+
+        //
+        //STAGE 1 inMemory
         List<Row> inMemory = new ArrayList<Row>();
 
         inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
@@ -41,7 +46,7 @@ public class module_66_SparkSQL_grouping {
                 new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
         };
         StructType schema = new StructType(fields);
-        */
+
         /*Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
 
         dataset.createOrReplaceTempView("logging_table");
@@ -51,17 +56,22 @@ public class module_66_SparkSQL_grouping {
 
         // STAGE 2 large log file
         Dataset<Row> dataset = spark.read().option("header","true").csv("src/main/resources/biglog.txt");
-        dataset.show(200);
+        //dataset.show(10);
 
         dataset.createOrReplaceTempView("logging_table");
 
-        Dataset<Row> results = spark.sql("SELECT level, date_format(datetime,'MMMM') as month, count(1) FROM logging_table");
+        // ERROR HERE
+        //Dataset<Row> results = spark.sql("SELECT level, date_format(datetime,'MMMM') as month, count(1) FROM logging_table");
+        Dataset<Row> results = spark.sql("SELECT level, date_format(datetime, 'MMMM') as month FROM logging_table ");
         //System.out.println(results.count());
 
-        results.createOrReplaceTempView("results_table");
+        results.createOrReplaceTempView("logging_table");
 
-        Dataset<Row> totals = spark.sql("SELECT sum(total) FROM results_table");
-        totals.show();
+        results = spark.sql("SELECT level, month, count(1) as total FROM logging_table GROUP BY level, month");
+        results.show();
+
+        //Dataset<Row> totals = spark.sql("SELECT sum(total) FROM results_table");
+        //totals.show();
 
         //hack for keeping Spark UI 4040 running
         Scanner scanner = new Scanner(System.in);
